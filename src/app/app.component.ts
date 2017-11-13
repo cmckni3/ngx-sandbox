f6b3a8defad9fdaca683c09b92e325cbb0ee5ac3
Chris McKnight <cmckni3@gmail.com>
2017-11-12 23:19:23 -0600

    chore(rxjs): Playing with RxJS pipelines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { map } from 'rxjs/operators/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/never';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    const o = Observable.create(obs => {
+      for (let i = 1; i < 5; i = i + 1) {
+        obs.next(i);
+        if (i % 2 === 0) {
+          obs.error('Foo');
+        }
+      }
+    });
+
+    o
+    .pipe(
+      map((n: number) => n * 2),
+    )
+    .catch(err => {
+      console.log(err);
+      return Observable.never();
+    })
+    .subscribe(x => console.log(x));
+  }
 }
